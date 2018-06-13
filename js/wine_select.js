bbaf621aa9ab446f3522194add21298aa21f0c3d
Hanlin Chen <hchen47@ucsc.edu>
2018-06-13 15:54:46 -0700

    Updated

--- js/wine_select.js
+++ js/wine_select.js
@@ -1,13 +1,16 @@
+
 $(document).ready(function(){
   var margin = {top: 20, right: 80, bottom: 30, left: 50},
-      width = 500 - margin.left - margin.right,
+      width = 600 - margin.left - margin.right,
       height = 500 - margin.top - margin.bottom;
+  
+
+  var parseTime= d3.timeParse("%Y")
 
   var x = d3.scaleLinear()
       .range([0, width]);
 
-  var y = d3.scaleLinear()
-      .range([0, height]);
+  var y = d3.scaleTime().range([0, height])
 
   var r = d3.scaleSqrt()
           .range([2,10]);
@@ -37,7 +40,9 @@ $(document).ready(function(){
   d3.csv('WineDataSet.csv', function(error, data){
       data.forEach(function(d){
       d.points = +d.points;
-      d.year  = +d.year;
+      d.year1  = d.year;
+      d.price  = d.price;
+      d.year  = parseTime(d.year);
       });
 
       x.domain(d3.extent(data, function(d){
@@ -46,19 +51,19 @@ $(document).ready(function(){
 
       y.domain(d3.extent(data, function(d){
           return d.year;
-      })).nice();
+      }));
 
       r.domain(d3.extent(data, function(d){
           return d.year;
       })).nice();
     
     svg.append('g')
-      .attr('transform', 'translate(0,' + (height) + ')')
+      .attr('transform', 'translate(-3,' + (height+5) + ')')
       .attr('class', 'x axis')
       .call(xAxis);
 
     svg.append('g')
-      .attr('transform', 'translate(0,0)')
+      .attr('transform', 'translate(-7,0)')
       .attr('class', 'y axis')
       .call(yAxis);
 
@@ -78,55 +83,40 @@ $(document).ready(function(){
     // we use the ordinal scale symbols to generate symbols
     // such as d3.symbolCross, etc..
     // -> symbol.type(d3.symbolCross)()
-      svg.selectAll(".symbol")
-      .data(data)
-    .enter().append("path")
-      .attr("class", "symbol")
-      .attr("d", function(d, i) { return symbol.type(symbols(d.primary))(); })
-      .style("fill", function(d) { return color(d.primary); })
-      .attr("transform", function(d) { 
-        return "translate(" + x(d.points) + "," + y(d.year) +")"; 
-      });
-    
-    var clicked = ""
-    
-    var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(75," + i * 20 + ")"; });
-
-    legend.append("path")
-      .style("fill", function(d) { return color(d); })
-          .attr("d", function(d, i) { return symbol.type(symbols(d))(); })
-          .attr("transform", function(d, i) { 
-              return "translate(" + (width-15) + "," + 10 + ")";
+      var circles = svg.selectAll(".dot")
+          .data(data)
+          .enter().append("circle")
+          .attr("class", "dot")
+          .attr("r", function(d) { return 5; })
+          .attr("cx", function(d) {return x(d.points);})
+          .attr("cy", function(d) {return y(d.year);})
+          .style("fill", function (d) { return color(d.primary); })
+          .on("mouseover", function(d) {
+              tooltip.transition()
+              .duration(200)
+              .style("opacity", 1);
+              tooltip.html( d.title + 
+                  "<br/>" + "Vintage Year:" + d.year1 + 
+                  "<br/>" + "Price(USD): $" + d.price + 
+                  "<br/>" + "Variety: " + d.variety + 
+                  "<br/>" + "Place of Origin: " + d.country + " ("+d.province + ") "+  
+                  "<br/> " + "Expert Grade(out of 100pts): " + d.points + "")
+              .style("left", (d3.event.pageX -120) + "px")
+              .style("top", (d3.event.pageY-30) + "px");
           })
-          .on("click",function(d){
-             d3.selectAll(".symbol").style("opacity",1)
-             
-             if (clicked !== d){
-               d3.selectAll(".symbol")
-                 .filter(function(e){
-                 return e.primary !== d;
-               })
-                 .style("opacity",0.1)
-               clicked = d
-             }
-              else{
-                clicked = ""
-              }
-          });
-   
-    legend.append("text")
-        .attr("x", width - 24)
-        .attr("y", 9)
-        .attr("dy", ".35em")
-        .style("text-anchor", "end")
-        .text(function(d) { return d; });
+          .on("mouseout", function(d) {
+              tooltip.transition()
+              .duration(200)
+              .style("opacity", 0);
+          });  
+
+          var tooltip = d3.select("body").append("div")
+              .attr("class",  "tooltip") 
+              .style("opacity", 0); 
+    var clicked = ""
 
   });
-$(document).ready(function(){
+
   var margin1 = {top: 20, right: 80, bottom: 30, left: 50},
       width1 = 1000 - margin1.left - margin1.right,
       height1 = 650 - margin1.top - margin1.bottom;
@@ -277,7 +267,5 @@ $(document).ready(function(){
                     })
             }
         }
-});
-
 
 });
