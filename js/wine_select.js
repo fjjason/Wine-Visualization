2fb38041f2db44bed2ebb3f0ed3178204e3b3b4d
Hanlin Chen <hchen47@ucsc.edu>
2018-06-14 13:14:20 -0700

    done plus done

--- js/wine_select.js
+++ js/wine_select.js
@@ -234,7 +234,7 @@ $(document).ready(function() {
                             ''
                     )
                     .style('left', d3.event.pageX - 230 + 'px')
-                    .style('top', d3.event.pageY + 'px');
+                    .style('top', d3.event.pageY -120 + 'px');
             })
             .on('mouseout', function(d) {
                 tooltip
